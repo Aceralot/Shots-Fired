2edf76d4adbd78ce6855dc15f4e91e3737a377bc
AC Harold <aceralot@gmail.com>
2026-05-02 02:03:02 -0700

    Character Moves and Shoots
    I did not configure the mouse clicks to shoot or look around yet.

---
+++ Scripts/Player/gun.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func shoot():
+	const BULLET = preload("res://Scripts/Player/Bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child( new_bullet )
+	
+
+func _on_timeout() -> void:
+	shoot()
---
+++ Scripts/player_area_2d.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+var travelled_distance = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	const SPEED = 1000
+	const RANGE = 1200
+	var direction = Vector2.RIGHT.rotated( rotation )
+	position += direction * SPEED * delta
+	
+	travelled_distance += SPEED * delta
+	if travelled_distance > RANGE:
+		queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	queue_free()
+	if body.has_method( "take_damage" ):
+		body.take_damage
+	pass # Replace with function body.
