7d9c98b3f0220ea09c543e20eebece6c0f6a383e
AC Harold <aceralot@gmail.com>
2026-05-05 20:34:46 -0700

    Fixed Scripts added
    There is some sort of bug but I don't know how to find it.

--- Scripts/UI/controls.gd
+++ Scripts/UI/controls.gd
@@ -6,10 +6,10 @@ extends VBoxContainer
 @export var right_button: Button
 
 const ACTIONS = {
-"up": "move_up",
-"down": "move_down",
-"left": "move_left",
-"right": "move_right" 
+	"up": "move_up",
+	"down": "move_down",
+	"left": "move_left",
+	"right": "move_right"
 }
 
 var waiting_for_input: String = ""
@@ -17,12 +17,12 @@ var waiting_for_input: String = ""
 func _ready():
 	_update_button_labels()
 
-	up_button.pressed.connect( _on_rebind_button_pressed.bind( "up"))
-	down_button.pressed.connect( _on_rebind_button_pressed.bind( "down"))
-	left_button.pressed.connect( _on_rebind_button_pressed.bind( "left"))
-	right_button.pressed.connect( _on_rebind_button_pressed.bind( "right"))
+	up_button.pressed.connect(_on_rebind_button_pressed.bind("up"))
+	down_button.pressed.connect(_on_rebind_button_pressed.bind("down"))
+	left_button.pressed.connect(_on_rebind_button_pressed.bind("left"))
+	right_button.pressed.connect(_on_rebind_button_pressed.bind("right"))
 
-func _get_button( direction: String ) -> Button:
+func _get_button(direction: String) -> Button:
 	match direction:
 		"up": return up_button
 		"down": return down_button
@@ -30,9 +30,9 @@ func _get_button( direction: String ) -> Button:
 		"right": return right_button
 		_: return null
 
-func _update_button_label( direction: String ):
+func _update_button_label(direction: String):
 	var action_name = ACTIONS[direction]
-	var events = InputMap.action_get_events( action_name )
+	var events = InputMap.action_get_events(action_name)
 
 	var label_text = "Unassigned"
 
@@ -40,36 +40,35 @@ func _update_button_label( direction: String ):
 		var event = events[0]
 		if event is InputEventKey:
 			if event.keycode != 0:
-				label_text = OS.get_keycode_string( event.keycode )
+				label_text = OS.get_keycode_string(event.keycode)
 			elif event.physical_keycode != 0:
-				label_text = OS.get_keycode_string( event.physical_keycode )
+				label_text = OS.get_keycode_string(event.physical_keycode)
 
-	var btn = _get_button( direction )
+	var btn = _get_button(direction)
 	btn.text = "%s" % [label_text]
 
 func _update_button_labels():
 	for dir in ACTIONS.keys():
-		_update_button_label( dir )
+		_update_button_label(dir)
 
-func _on_rebind_button_pressed( direction: String ):
+func _on_rebind_button_pressed(direction: String):
 	waiting_for_input = direction
-	var btn = _get_button( direction )
+	var btn = _get_button(direction)
 	btn.text = "..."
-	set_process( true )
+	set_process_input(true)
 
-func _input( event ):
+func _input(event):
 	if waiting_for_input == "":
 		return
 	
 	if event is InputEventKey and event.pressed:
 		var direction = waiting_for_input
-		var action_name = ACTIONS[ direction ]
+		var action_name = ACTIONS[direction]
 
-		InputMap.action_erase_events( action_name ) 
-		InputMap.action_add_event( action_name, event ) 
+		InputMap.action_erase_events(action_name)
+		InputMap.action_add_event(action_name, event)
 
-		_update_button_label( direction )
+		_update_button_label(direction)
 
 		waiting_for_input = ""
-		set_process_input( false )
-			
+		set_process_input(false)
